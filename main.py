f7ef21c9771ba37d4c157fd4a3302542188ab0d0
wojtazzz <wfijarczyk@gmail.com>
2023-10-09 14:30:15 +0000

    installed modelfarm

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print("Dupa")
-print("some more text")
-print("""
-some text
-some more text111
-lalalfdfkjgh
-the End!
-som emore changes
-day_1_100 branch
-""")
+from replit.ai.modelfarm import CompletionModel
+
+model = CompletionModel("text-bison")
+response = model.complete(["Write a tweet about the meaning of life: "], temperature=0.2)
+
+print(response.responses[0].choices[0].content)
